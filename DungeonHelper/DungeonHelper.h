51e4db89b749f29fff972340c2c8cfd97d89aa7a
Sean Monroe <seanmonroe86@gmail.com>
2018-06-06 20:45:00 -0500

    case skeleton ready for menu display and running

---
+++ DungeonHelper/DungeonHelper.h
@@ -0,0 +1,22 @@
+//=============================================================================
+// DungeonHelper.h
+//
+// Common defines, values, typedefs, etc for the Dungeon Helper project.
+//
+#ifndef DUNGEONHELPER_H_
+#define DUNGEONHELPER_H_
+
+//=============================================================================
+// Defines
+//
+#define SUCCESS				0
+#define ERROR_UNSPECIFIED	1
+
+//=============================================================================
+// Typedefs
+//
+typedef int STATUS;
+
+
+
+#endif // DUNGEONHELPER_H_
